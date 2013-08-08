07322394aad2c82cbeba6f4d123bf9463e35f219
Peter Bødskov <peter.boedskov@jyskebank.dk>
2013-08-08 15:21:35 +0200

    chapter 2 done, minus challenges

---
+++ GeoQuiz/src/main/java/dk/jyskebank/android/geoquiz/TrueFalse.java
@@ -0,0 +1,30 @@
+package dk.jyskebank.android.geoquiz;
+
+/**
+ * Created by jb3579 on 08/08/13.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion){
+        setQuestion(question);
+        setTrueQuestion(trueQuestion);
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
