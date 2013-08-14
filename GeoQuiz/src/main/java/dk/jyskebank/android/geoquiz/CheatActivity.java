67617d755eeefcbbe1f657bf1855627f4658ffb5
Peter Bødskov <peter.boedskov@jyskebank.dk>
2013-08-14 15:34:10 +0200

    chapter 5 done

---
+++ GeoQuiz/src/main/java/dk/jyskebank/android/geoquiz/CheatActivity.java
@@ -0,0 +1,48 @@
+package dk.jyskebank.android.geoquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by jb3579 on 14/08/13.
+ */
+public class CheatActivity extends Activity {
+
+    public static final String EXTRA_ANSWER_IS_TRUE = "dk.jyskebank.android.geoquiz.answer_is_true";
+    public static final String EXTRA_ANSWER_SHOWN = "dk.jyskebank.android.geoquiz.answer_shown";
+    public boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+        setAnswerShownResult(false);
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+        mShowAnswerButton = (Button) findViewById(R.id.showAnswerButton);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mAnswerIsTrue){
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
+            }
+        });
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown){
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+}
--- GeoQuiz/src/main/java/dk/jyskebank/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/main/java/dk/jyskebank/android/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package dk.jyskebank.android.geoquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -17,9 +18,11 @@ public class QuizActivity extends Activity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mCheatButton;
     private ImageButton mNextButton;
     private ImageButton mPreviousButton;
     private TextView mQuestionTextView;
+    private boolean mIsCheater;
 
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
             new TrueFalse(R.string.question_oceans, true),
@@ -59,6 +62,17 @@ public class QuizActivity extends Activity {
             }
         });
 
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+                intent.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+                startActivityForResult(intent, 0);
+            }
+        });
+
         mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +95,7 @@ public class QuizActivity extends Activity {
                 showNextQuestion();
             }
         });
+        mIsCheater = false;
     }
 
     private void showNextQuestion() {
@@ -104,7 +119,13 @@ public class QuizActivity extends Activity {
 
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
-        int messageResId = userPressedTrue == answerIsTrue ? R.string.correct_toast : R.string.incorrect_toast;
+        int messageResId = 0;
+        if(mIsCheater){
+            messageResId = R.string.judgment_toast;
+        } else {
+            messageResId = userPressedTrue == answerIsTrue ? R.string.correct_toast : R.string.incorrect_toast;
+        }
+
         Toast.makeText(QuizActivity.this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
@@ -149,5 +170,13 @@ public class QuizActivity extends Activity {
         super.onSaveInstanceState(outState);
         Log.d(TAG, "onSaveInstanceState() called");
         outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(data == null){
+            return;
+        }
+        mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
     }
 }
